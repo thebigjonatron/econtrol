d5008f9ef8e17859b72bb91e8757a1f3760e4884
vanderpluijmg <vanderpluijmgregory@gmail.com>
2023-05-01 12:41:52 +0100

    Calling flags

---
+++ src/cli.rs
@@ -0,0 +1,32 @@
+use clap::Parser;
+
+#[derive(Parser,Default,Debug)]
+#[clap(author="Author Name", version)]
+/// econtrol is a utility tool to read and write to embedded controller for msi laptops.
+struct Arguments {
+    #[clap(short, long)]
+    /// Enable or disable cooler booster
+    coolerbooster: bool,
+    #[clap(short, long)]
+    /// Print contents of embedded controller
+    print: bool,
+    #[clap(short, long)]
+    /// Switch fn and windows key
+    switchfn: bool,
+    #[clap(short, long)]
+    /// Write to embedded controller
+    write: bool,
+}
+
+pub fn parse_args(){
+    let args = Arguments::parse();
+    print!("{:?}", args);
+    if args.coolerbooster {
+        enable_coolerbooster();
+    }
+}
+
+fn enable_coolerbooster(){
+    println!("Hello World!")
+}
+
